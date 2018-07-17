e88cf7626cc59b50b585e96f33575d7f5d0e4296
Pagan Gazzard <page@resin.io>
2018-07-17 10:09:23 -0700

    Fixes for use on windows
    Change-type: patch

--- tests/tests.ts
+++ tests/tests.ts
@@ -3,14 +3,17 @@ import * as stream from 'stream';
 
 import * as TarUtils from '../lib';
 
+import * as path from 'path';
+
 describe('Simple utils', () => {
 	it('should correctly normalize a tar entry', (done) => {
+
 		const fn = TarUtils.normalizeTarEntry;
 		expect(fn('Dockerfile')).to.equal('Dockerfile');
 		expect(fn('./Dockerfile')).to.equal('Dockerfile');
-		expect(fn('../Dockerfile')).to.equal('../Dockerfile');
+		expect(fn('../Dockerfile')).to.equal(`..${path.sep}Dockerfile`);
 		expect(fn('/Dockerfile')).to.equal('Dockerfile');
-		expect(fn('./a/b/Dockerfile')).to.equal('a/b/Dockerfile');
+		expect(fn('./a/b/Dockerfile')).to.equal(`a${path.sep}b${path.sep}Dockerfile`);
 		done();
 	});
 
